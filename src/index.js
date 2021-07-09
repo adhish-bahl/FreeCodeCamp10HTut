85f6e557e98f294dbc0616f3bceef208dec3a6ff
Adhish Bahl <adhishbahl@LAPTOP-NFRM8UIQ.localdomain>
2021-07-09 21:22:48 +0530

    Function using arrow function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 // FIRST COMPONENT
-function FirstComponent () {
+// function FirstComponent () {
+//     return (
+//         <>
+//             <h1>Hello Friends!</h1>
+//             <h6>This is Adhish Bahl here!</h6>
+//         </>
+//     );
+// }
+// ReactDom.render(<FirstComponent/>, document.getElementById("root"));
+
+
+// ARROW FUNCTIONS AS COMPONENTS
+const ArrowFun = () => {
     return (
-        <>
-            <h1>Hello Friends!</h1>
-            <h6>This is Adhish Bahl here!</h6>
-        </>
+            React.createElement("h1", {}, "Hello Friends!")
+            // React.createElement("div", {}, React.createElement("h6", {}, "This is Adhish Bahl!"));
     );
 }
-ReactDom.render(<FirstComponent/>, document.getElementById("root"));
-
+ReactDom.render(<ArrowFun/>, document.getElementById("root"));
