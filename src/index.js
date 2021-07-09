454591bfa2b2cdf2e127343d45079d5dde41cfdc
adhish-bahl <adhishbahl0@gmail.com>
2021-07-09 22:23:45 +0530

    Amazon grid clone

--- src/index.js
+++ src/index.js
@@ -24,21 +24,60 @@ import ReactDom from 'react-dom';
 
 
 // Nested Components
-function Final () {
-    return (
-        <>
-            <DivH1/>
-            <DivH6/>
-        </>
-    );
-}
+// function Final () {
+//     return (
+//         <>
+//             <DivH1/>
+//             <DivH6/>
+//         </>
+//     );
+// }
     
-function DivH1 () {
-    return (
-        <h1>Hello Friends!</h1>
-    );
-}
+// function DivH1 () {
+//     return (
+//         <h1>Hello Friends!</h1>
+//     );
+// }
 
-const DivH6 = () => <h6>This is Adhish Bahl here!</h6>;
+// const DivH6 = () => <h6>This is Adhish Bahl here!</h6>;
 
-ReactDom.render(<Final/>, document.getElementById("root"));
+// ReactDom.render(<Final/>, document.getElementById("root"));
+
+
+// Amazon Grid Clone
+// function Books() {
+//     return(
+//     <>
+//         <Book/>
+//         <Book/>
+//         <Book/>
+//         <Book/>
+//         <Book/>
+//         <Book/>
+//     </>
+//     );
+// }
+
+// function Book() {
+//     return (
+//     <>
+//         <Image/>
+//         <BookName/>
+//         <Author/>
+//         <Price/>
+//     </>
+//     );
+// }
+
+
+// const Image = () => (
+//   <img src="https://m.media-amazon.com/images/I/41zoxjP9lcL._AC_UY327_FMwebp_QL65_.jpg" />
+// );
+
+// const BookName = () => <h2>Clean Code</h2>;
+
+// const Author = () => <h4>>by Robert C Martin</h4>;
+
+// const Price = () => <h3>₹950</h3>;
+
+// ReactDom.render(<Books/>,  document.getElementById("root"));
