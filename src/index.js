57654769f8001f15614beb2c1b5f01598458280f
adhish-bahl <adhishbahl0@gmail.com>
2021-07-10 18:22:44 +0530

    Done styling, spreading operator, props, destructuring, proper list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
 // FIRST COMPONENT
 // function FirstComponent () {
@@ -11,7 +11,7 @@ import ReactDom from 'react-dom';
 //     );
 // }
 // ReactDom.render(<FirstComponent/>, document.getElementById("root"));
-
+// --------------------------------------------------------------------------------------------------------------------------------------------
 
 // ARROW FUNCTIONS AS COMPONENTS
 // const ArrowFun = () => {
@@ -21,7 +21,7 @@ import ReactDom from 'react-dom';
 //     );
 // }
 // ReactDom.render(<ArrowFun/>, document.getElementById("root"));
-
+// --------------------------------------------------------------------------------------------------------------------------------------------
 
 // Nested Components
 // function Final () {
@@ -32,52 +32,95 @@ import ReactDom from 'react-dom';
 //         </>
 //     );
 // }
-    
+
 // function DivH1 () {
 //     return (
 //         <h1>Hello Friends!</h1>
 //     );
 // }
-
 // const DivH6 = () => <h6>This is Adhish Bahl here!</h6>;
 
 // ReactDom.render(<Final/>, document.getElementById("root"));
-
+// --------------------------------------------------------------------------------------------------------------------------------------------
 
 // Amazon Grid Clone
+// function Books() {
+//     return <section>
+//         <Book/>
+//         <Book/>
+//         <Book/>
+//     </section>;
+// }
+
+// function Book() {
+//     return (
+//         <section>
+//             <Image />
+//             <BookName />
+//             <Author />
+//             {/* <Price/> */}
+//         </section>
+//     );
+// }
+
+// const Image = () => (
+//     <img
+//         src="https://m.media-amazon.com/images/I/41zoxjP9lcL._AC_UY327_FMwebp_QL65_.jpg"
+//     />
+// );
+// const BookName = () => <h2>Clean Code</h2>;
+// const Author = () => <h4>by Robert C Martin</h4>;
+// const Price = () => <h3>₹950</h3>;
+
+// ReactDom.render(<Books />, document.getElementById("root"));
+// --------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// PROPS IN REACT, USED IN AMAZON CLONE
+const data = [
+    {
+        id: 1,
+        img: "https://m.media-amazon.com/images/I/41zoxjP9lcL._AC_UY327_FMwebp_QL65_.jpg",
+        title: "Clean Code",
+        author: "Robert C Martin",
+    },
+    {
+        id: 2,
+        img: "https://m.media-amazon.com/images/I/81RkzIXMFxL._AC_UY327_FMwebp_QL65_.jpg",
+        title: "Grokking Algorithms",
+        author: "Aditya Bhargava ",
+    },
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/91asIC1fRwL._AC_UY327_FMwebp_QL65_.jpg",
+        title: "Eloquent JavaScript",
+        author: "Marijn Haverbeke ",
+    },
+];
+
 function Books() {
-    return(
-    <>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-    </>
-    );
-}
-
-function Book() {
     return (
-    <>
-        <Image/>
-        <BookName/>
-        <Author/>
-        <Price/>
-    </>
+        <section className="container">
+            {data.map((data) => {       // Making Proper List
+                return (
+                    //<Book key={data.id} book={book} />
+                    <Book key={data.id} {...data} /> // Seperation Operator        // key for all
+                );
+            })}
+        </section>
     );
 }
 
+function Book(props) {      //Props
+    // const {img, title, author} = props.book;
+    const { img, title, author } = props;       // Destructuring
+    return (
+        <section className="book">
+            <img src={img} />
+            <h1>{title}</h1>
+            <h4>{author}</h4>
+        </section>
+    );
+}
 
-const Image = () => (
-  <img src="https://m.media-amazon.com/images/I/41zoxjP9lcL._AC_UY327_FMwebp_QL65_.jpg" />
-);
-
-const BookName = () => <h2>Clean Code</h2>;
-
-const Author = () => <h4>>by Robert C Martin</h4>;
-
-const Price = () => <h3>₹950</h3>;
-
-ReactDom.render(<Books/>,  document.getElementById("root"));
+ReactDom.render(<Books />, document.getElementById("root"));
